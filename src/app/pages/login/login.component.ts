92d7ccaca9388ffca23d0725541bfc6a82c531b6
julian <estrotococoa@gmail.com>
2020-09-14 11:24:31 -0300

    alerts implemented

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { UserModel } from 'src/app/models/user.models';
 import { AuthService } from 'src/app/services/auth.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-login',
@@ -22,13 +23,27 @@ export class LoginComponent implements OnInit {
     if ( form.invalid ) {
       return;
     }
+
+    Swal.fire({
+      allowOutsideClick: false,
+      title: 'Info',
+      text: 'Procesando...',
+      icon: 'info'
+    });
+    Swal.showLoading(); 
     
     this.auth.login( this.user )
       .subscribe(res => {
         console.log(res);
+        Swal.close();
         
       }, (err) => {
         console.log(err.error.error.message);
+        Swal.fire({
+          title: 'Error',
+          text: err.error.error.message,
+          icon: 'error'
+        });
         
       })
     
--- src/app/pages/registro/registro.component.ts
+++ src/app/pages/registro/registro.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { UserModel } from 'src/app/models/user.models';
 import { NgForm } from '@angular/forms';
 import { AuthService } from 'src/app/services/auth.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-registro',
@@ -23,13 +24,27 @@ export class RegistroComponent implements OnInit {
     if ( form.invalid ) {
       return;
     }
+
+    Swal.fire({
+      allowOutsideClick: false,
+      title: 'Info',
+      text: 'Procesando...',
+      icon: 'info'
+    });
+    Swal.showLoading(); 
+
     this.auth.registerNewUser( this.usuario )
       .subscribe( resp => {
         console.log(resp);
+        Swal.close();
         
       }, (err) => {
         console.log(err.error.error.message);
-        
+        Swal.fire({
+          title: 'Error',
+          text: err.error.error.message,
+          icon: 'error'
+        });
       })
     
     
